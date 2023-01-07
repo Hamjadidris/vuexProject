348838f90c1d0e53d22d28f76e4f54f74ba82f04
Hamjad Idris <dajmahsirdi@gmail.com>
2023-01-07 23:15:51 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script >
+import AddTodo from './components/AddTodo.vue';
+import FilterTodos from './components/FilterTodos.vue';
+import Todos from './components/Todos.vue';
+
+export default {
+  name: "App",
+  components: {
+    Todos,
+    AddTodo,
+    FilterTodos
+  }
+}
+</script>
+
+<template>
+  <div class="App">
+    <div class="container">
+      <AddTodo />
+      <FilterTodos />
+      <Todos />
+
+    </div>
+  </div>
+</template>
+
+<style >
+body {
+  font-family: "Franklin Gothic Medium", "Arial Narrow", "Arial", "sans-serif";
+  line-height: 1.6;
+  background: #e8f7f0;
+}
+
+.container {
+  max-width: 1100px;
+  margin: auto;
+  overflow: auto;
+  padding: 0 2rem;
+}
+</style>
